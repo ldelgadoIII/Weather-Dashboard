128e4028a68861016b6cd7012f06ee53139d7400
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-01-26 23:46:46 -0600

    added an init function to load up page and default data

--- fiveDay.js
+++ fiveDay.js
@@ -1,50 +1,50 @@
-// DEPENDENCES ==========================
-let fiveDayDisplay = $(".fiveDay");
-let icon;
+// // DEPENDENCES ==========================
+// let fiveDayDisplay = $(".fiveDay");
+// let icon;
 
-// FUNCTIONS ============================
-// Request five-day weather info
-function callFiveDay() {
-  let fiveDayURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    city +
-    "&appid=" +
-    APIKey +
-    "&units=imperial";
+// // FUNCTIONS ============================
+// // Request five-day weather info
+// function callFiveDay() {
+//   let fiveDayURL =
+//     "https://api.openweathermap.org/data/2.5/forecast?q=" +
+//     city +
+//     "&appid=" +
+//     APIKey +
+//     "&units=imperial";
 
-  $.ajax({
-    url: fiveDayURL,
-    method: "GET",
-  }).then(function (response) {
-    displayFiveDay(response);
-  });
-}
+//   $.ajax({
+//     url: fiveDayURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     displayFiveDay(response);
+//   });
+// }
 
-// Display five day forecast cards
-function displayFiveDay(response) {
-  for (let i = 0; i < 40; i += 8) {
-    // Set cloudiness icon
-    if (response.list[i].clouds.all <= 30) {
-      icon = "fa-sun";
-    } else if (response.list[i].clouds.all <= 90) {
-      icon = "fa-cloud-sun";
-    } else {
-      icon = "fa-cloud";
-    }
+// // Display five day forecast cards
+// function displayFiveDay(response) {
+//   for (let i = 0; i < 40; i += 8) {
+//     // Set cloudiness icon
+//     if (response.list[i].clouds.all <= 30) {
+//       icon = "fa-sun";
+//     } else if (response.list[i].clouds.all <= 90) {
+//       icon = "fa-cloud-sun";
+//     } else {
+//       icon = "fa-cloud";
+//     }
 
-    // Create five-day forecast cards
-    let newDiv = $('<div class= "col-md-2">');
-    newDiv.html(`<div class="card text-white bg-primary mb-3" style="max-width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">Primary card title</h5>
-        <i class="fas ${icon} fa-7x icons"></i>  
-        <div class="card-text">
-          Temp: ${response.list[i].main.temp}
-          </div>
-          <div class="card-text">
-          Humidity: ${response.list[i].main.humidity}
-          </div>
-      </div>`);
-    fiveDayDisplay.append(newDiv);
-  }
-}
+//     // Create five-day forecast cards
+//     let newDiv = $('<div class= "col-md-2">');
+//     newDiv.html(`<div class="card text-white bg-primary mb-3" style="max-width: 18rem">
+//       <div class="card-body">
+//         <h5 class="card-title">Primary card title</h5>
+//         <i class="fas ${icon} fa-7x icons"></i>
+//         <div class="card-text">
+//           Temp: ${response.list[i].main.temp}
+//           </div>
+//           <div class="card-text">
+//           Humidity: ${response.list[i].main.humidity}
+//           </div>
+//       </div>`);
+//     fiveDayDisplay.append(newDiv);
+//   }
+// }
--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 // DEPENDENCES ==========================
 let weatherDetails = $(".weather-details");
 let cityNameHeader = $(".cityName");
+let fiveDayDisplay = $(".fiveDay");
+let icon;
 
 // STARTING DATA ========================
-let city = "Chicago";
-let lat = 41.85;
-let lon = -87.65;
+let city;
+let lat;
+let lon;
 let APIKey = "27c3f1b88c0950432250ead2579926a4";
 
 // FUNCTIONS ============================
+function init() {
+  // check if there is anything stored in local storage
+  // Set default values
+  city = "Chicago";
+  lat = 41.85;
+  lon = -87.65;
+  cityNameHeader.html(city + " (1/26/2021)");
+
+  // Call displays
+  callWeather();
+  callUV();
+  callFiveDay();
+}
+
 // Request current weather info
 function callWeather() {
   let queryURL =
@@ -22,7 +38,6 @@ function callWeather() {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     lat = response.coord.lat;
     lon = response.coord.lon;
     displayMainWeather(response);
@@ -71,6 +86,51 @@ function displayUV(response) {
   }
 }
 
-// USER INTERACTIONS ====================
+// Request five-day weather info
+function callFiveDay() {
+  let fiveDayURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+
+  $.ajax({
+    url: fiveDayURL,
+    method: "GET",
+  }).then(function (response) {
+    displayFiveDay(response);
+  });
+}
+
+// Display five day forecast cards
+function displayFiveDay(response) {
+  for (let i = 0; i < 40; i += 8) {
+    // Set cloudiness icon
+    if (response.list[i].clouds.all <= 30) {
+      icon = "fa-sun";
+    } else if (response.list[i].clouds.all <= 90) {
+      icon = "fa-cloud-sun";
+    } else {
+      icon = "fa-cloud";
+    }
+
+    // Create five-day forecast cards
+    let newDiv = $('<div class= "col-md-2">');
+    newDiv.html(`<div class="card text-white bg-primary mb-3" style="max-width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">Primary card title</h5>
+        <i class="fas ${icon} fa-7x icons"></i>  
+        <div class="card-text">
+          Temp: ${response.list[i].main.temp}
+          </div>
+          <div class="card-text">
+          Humidity: ${response.list[i].main.humidity}
+          </div>
+      </div>`);
+    fiveDayDisplay.append(newDiv);
+  }
+}
+
 // INITIALIZATION =======================
-// check if there is anything stored in local storage
+init();
