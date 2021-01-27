6838680877e09fbaa97069a0cd93906b20bdad2b
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-01-26 22:39:54 -0600

    added a feature that accurately outputs the temp data for the correct day in the five-day forecast cards

--- fiveDay.js
+++ fiveDay.js
@@ -7,7 +7,8 @@ let fiveDayURL =
   "https://api.openweathermap.org/data/2.5/forecast?q=" +
   city +
   "&appid=" +
-  APIKey;
+  APIKey +
+  "&units=imperial";
 
 // FUNCTIONS ============================
 // Request five-day weather info
@@ -21,12 +22,12 @@ $.ajax({
 
 // Display five day forecast cards
 function displayFiveDay(response) {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 40; i += 8) {
     // Set cloudiness icon
-    if (response.list[0].clouds.all <= 30) {
+    if (response.list[i].clouds.all <= 30) {
       icon = "fa-sun";
-    } else if (response.list[0].clouds.all <= 60) {
-      icon = "fa-sun-cloud";
+    } else if (response.list[i].clouds.all <= 90) {
+      icon = "fa-cloud-sun";
     } else {
       icon = "fa-cloud";
     }
@@ -38,10 +39,10 @@ function displayFiveDay(response) {
         <h5 class="card-title">Primary card title</h5>
         <i class="fas ${icon} fa-7x icons"></i>  
         <div class="card-text">
-          Temp: ${response.list[0].main.temp}
+          Temp: ${response.list[i].main.temp}
           </div>
           <div class="card-text">
-          Humidity: ${response.list[0].main.humidity}
+          Humidity: ${response.list[i].main.humidity}
           </div>
       </div>`);
     fiveDayDisplay.append(newDiv);
